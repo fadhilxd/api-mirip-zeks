3ed9f2aab067f7b21d8276dc0b2128eabe68b808
FadhilXD <133642821+fadhilxd@users.noreply.github.com>
2023-05-22 13:16:53 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'FadhilXD' 
+global.apikey = ["FadhilXD", "Alphabot", "Alpha"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
